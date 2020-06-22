d8d5c161306af62327b2fe83196a917806a8cc7a
Dina <diana.liubarets@hotmail.com>
2020-06-22 18:45:37 -0400

    script add

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var startTime = currentTime - 600000
 //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
 var options = {
   host: 'api.coincap.io',
-  path: '/v2/assets/'+ coin +'/history?interval=m1&start='+ startTime +'&end='+ currentTime,
+  path: '/v2/assets/bitcoin/history?interval=m1&start='+ startTime +'&end='+ currentTime,
   method: 'GET'
 };
 
